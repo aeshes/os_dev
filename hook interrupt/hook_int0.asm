34c297f992e2fe29b95ca93723907546b506b331
Arthur Domosedov <mathesis@inbox.ru>
2019-07-20 15:38:49 +0300

    Update hook_int0.asm

--- hook interrupt/hook_int0.asm
+++ hook interrupt/hook_int0.asm
@@ -7,7 +7,7 @@ org 7c00h
   mov ax, [si+2]
   mov bx, [si]
   mov [original_vector+3], ax
-  mov[original_vector+1], bx
+  mov [original_vector+1], bx
   xor ax,  ax
   mov bx, int_proc
   xor si, si
